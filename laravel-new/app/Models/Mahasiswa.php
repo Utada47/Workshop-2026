2d8fc7b8faacd95c9a95f68c6273b5951d3a6de7
Utada47 <bintangwijaya0021@gmail.com>
2026-07-24 23:19:26 +0800

    add mahasiswa model

--- laravel-new/app/Models/Mahasiswa.php
+++ laravel-new/app/Models/Mahasiswa.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'mst_mahasiswa'; // nama tabel tidak mengikuti konvensi default ("mahasiswas")
+
+    protected $fillable = [
+        'stambuk', 'name', 'jurusan',
+    ];
+
+    public function angkatan(): string
+    {
+        // 4 digit pertama stambuk dianggap sebagai tahun angkatan, mis. "20210011" -> "2021"
+        return substr($this->stambuk, 0, 4);
+    }
 }
